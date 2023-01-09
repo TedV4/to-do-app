1b27e3a441631f73dda3de09d60a0ae226c06e87
TedV4 <tedverdonkschot@outlook.com>
2023-01-09 16:07:13 -0700

    Add functions for creating a new project card and displaying it in the DOM. Will refactor.

---
+++ app.js
@@ -0,0 +1,76 @@
+//Projects storage
+let projects = [];
+const projectContainer = document.getElementById("projectContainer");
+
+class Project {
+  constructor(name, index) {
+    this.name = name;
+    this.index = index;
+  }
+
+  tasks = [];
+
+  get tasks() {
+    return this.tasks;
+  }
+
+  addTask(task) {
+    this.tasks.push(task);
+  }
+}
+
+//throw this in a dom control class
+function clearInputField(inputID) {
+  document.getElementById(inputID).value = "";
+}
+
+//clear project container
+function clearProjectContainer() {
+  while (projectContainer.firstChild) {
+    projectContainer.removeChild(projectContainer.firstChild);
+  }
+}
+
+const submitProjectBtn = document.getElementById("submitProjectBtn");
+
+//add project to projects array
+function addProjectToStorage() {
+  const projectName = document.getElementById("projectNameInput").value;
+  const project = new Project(projectName, projects.length);
+  projects.push(project);
+}
+
+//add all projects to DOM
+function addProjectsToDOM() {
+  for (let i = 0; i < projects.length; i++) {
+    const projectCard = document.createElement("div");
+    projectCard.classList.add("projectCard");
+
+    const projectName = document.createElement("h1");
+    projectName.textContent = projects[i].name;
+    projectCard.appendChild(projectName);
+
+    const taskHeaders = document.createElement("div");
+    taskHeaders.classList.add("taskHeaders");
+    projectCard.appendChild(taskHeaders);
+
+    const taskHeader = document.createElement("p");
+    taskHeader.textContent = "Tasks: ";
+    taskHeaders.appendChild(taskHeader);
+
+    const dueDateHeader = document.createElement("p");
+    dueDateHeader.textContent = "Due Date: ";
+    taskHeaders.appendChild(dueDateHeader);
+
+    projectContainer.appendChild(projectCard);
+  }
+}
+
+//throw this in a dom control class
+submitProjectBtn.addEventListener("click", function (e) {
+  e.preventDefault();
+  addProjectToStorage();
+  clearProjectContainer();
+  addProjectsToDOM();
+  clearInputField("projectNameInput");
+});
